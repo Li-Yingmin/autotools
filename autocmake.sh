ab1741a3d62fb8ecd7190064315435cec9bedde7
liyingmin <yingmin.li@hobot.cc>
2018-10-23 20:42:35 +0800

    fix autocmake.sh log

--- autocmake.sh
+++ autocmake.sh
@@ -3,16 +3,20 @@ CURRENT_DIR=$(pwd)
 cd $CURRENT_DIR
 
 if [ -d "build/" ];then
-  echo "-rm build dir..."
+  echo "-- [rm build dir build...]"
   rm -rf build
   mkdir build
   cd build
+  echo "-- [cmake ..]"
   cmake ..
+  echo "-- [make -j4]"
   make -j4
 else
-  echo "-create build dir..."
+  echo "-- create build dir..."
   mkdir build
   cd build
+  echo "-- [cmake ..]"
   cmake ..
+  echo "-- [make -j4]"
   make -j4
 fi
