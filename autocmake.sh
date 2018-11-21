39a67ccff4ee70a5bb0c46c417aed51d1641ffa8
Parallels <parallels@localhost.localdomain>
2018-11-21 16:18:34 +0800

    add chmod gradlew

--- autocmake.sh
+++ autocmake.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 CURRENT_DIR=$(pwd)
 cd $CURRENT_DIR
+echo "-- sh [exec chmod +x gradlew]"
+chmod +x gradlew
+
 echo "-- sh [exec gradlew]"
 ./gradlew
+
 
 if [ -d "build/" ];then
   echo "-- sh [rm build dir build...]"
