f69d7c9cd40c650a13b9fb354d93a5fe446aeb32
李英民 <yingmin.li@horizon.ai>
2019-02-13 11:29:59 +0800

    简化脚本

--- autocmake.sh
+++ autocmake.sh
@@ -9,28 +9,14 @@ echo "-- sh [exec gradlew]"
 
 multi="$1"
 
-if [[ -d "build/" ]];then
-  echo "-- sh [rm build dir build...]"
-  rm -rf build
-  rm -rf output
-  rm -rf *.tmp
-  rm -rf .gradle
-  mkdir build
-  cd build
-  echo "-- sh [cmake ..]"
-  cmake ..
-  echo "-- sh [make]"
-  make ${multi}
-else
-  echo "-- sh [create build dir...]"
-  rm -rf build
-  rm -rf output
-  rm -rf *.tmp
-  rm -rf .gradle
-  mkdir build
-  cd build
-  echo "-- sh [cmake ..]"
-  cmake ..
-  echo "-- sh [make]"
-  make ${multi}
-fi
+echo "-- sh [rm old dir...]"
+rm -rf build
+rm -rf output
+rm -rf *.tmp
+rm -rf .gradle
+mkdir build
+cd build
+echo "-- sh [cmake ..]"
+cmake ..
+echo "-- sh [make]"
+make ${multi}
